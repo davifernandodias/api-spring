149245db15bdc852db05af9ebb77de93a51b9b4f
davi dias <davifernandosousadias18@Gmail.com>
2024-11-14 00:52:39 -0300

    fix: change allow perms to front-end

--- src/main/java/com/projet/task/config/WebConfig.java
+++ src/main/java/com/projet/task/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200/") 
+                .allowedOrigins("https://to-do-list-task-processo-seletivo.vercel.app") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
